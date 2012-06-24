07fa914c32086a99c6ae1bf389d4eb6db6865747
Jason Madden <jason.madden@nextthought.com>
2012-06-24 18:29:12 +0000

    Mostly switch over from our ad-hoc and, it turns out, incorrect container layout; actually persist IRoot and ISite in the proper places. This helps with traversal and should help eliminate a lot of log warnings and hacked-together URL fixup code. Also switch from pyramid traversal to zope traversal because the latter is more strict and won't let us get away with the sorts of errors we have been. Still needs some more work.

--- src/nti/app/pyramid_zope/traversal.py
+++ src/nti/app/pyramid_zope/traversal.py
@@ -2,9 +2,21 @@
 
 from pyramid import traversal
 
-resource_path = traversal.resource_path
+#resource_path = traversal.resource_path
 find_interface = traversal.find_interface
 lineage = traversal.lineage
+
+from zope.location import traversing as ztraversing
+from zope.location import interfaces as loc_interfaces
+
+import urllib
+
+def resource_path( res ):
+	# This function is somewhat more flexible than pyramids, and
+	# also more strict. It requires strings (not None, for example)
+	# and bottoming out an at IRoot. This helps us get things right.
+	# It is probably also a bit slower.
+	return urllib.quote( loc_interfaces.ILocationInfo( res ).getPath() )
 
 def normal_resource_path( res ):
 	"""
@@ -16,8 +28,8 @@ def normal_resource_path( res ):
 	"""
 	# If this starts to get complicated, we can take a dependency
 	# on the urlnorm library
-	result = traversal.resource_path( res )
+	result = resource_path( res )
 	result = result.replace( '//', '/' )
 	# Our LocalSiteManager is sneaking in here, which we don't want...
-	result = result.replace( '%2B%2Betc%2B%2Bsite/', '' )
+	#result = result.replace( '%2B%2Betc%2B%2Bsite/', '' )
 	return result
