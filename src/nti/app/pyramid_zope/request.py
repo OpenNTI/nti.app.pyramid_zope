3ac8f05db463c187fe8757467eb72e8340063733
Jason Madden <jason.madden@nextthought.com>
2013-10-31 21:51:57 +0000

    Improve our request proxy and add some basic tests; it may have had something to do with the coppa form problems.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.pyramid_zope/trunk@26496 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/pyramid_zope/request.py
+++ src/nti/app/pyramid_zope/request.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Partial support for making a Pyramid request object work more
+Partial support for making a Pyramid request/response object pair work more
 like a Zope request.
+
+Partially based on ideas from :mod:`pyramid_zope_request`
 
 $Id$
 """
@@ -15,33 +17,74 @@ logger = __import__('logging').getLogger(__name__)
 from zope import interface
 from zope import component
 from zope.i18n.locales import locales
+from zope.security.management import getInteraction
+from zope.security.interfaces import NoInteraction
+from zope.authentication.interfaces import IUnauthenticatedPrincipal
 
 from pyramid.i18n import get_locale_name
 import pyramid.interfaces
-
 
 from zope.proxy import non_overridable, getProxiedObject
 from zope.proxy.decorator import SpecificationDecoratorBase
 import zope.publisher.interfaces.browser
 import zope.publisher.browser
 #import zope.publisher.interfaces.http # For IHTTPResponse, should we need it
-import operator
 
-@interface.implementer(zope.publisher.interfaces.browser.IBrowserRequest)
+from nti.utils.property import alias
+
+# Implement the request
+# and the "skin". In zope, the skin is changeable (IBrowserRequest
+# implements ISkinnable), especially
+# through the ++skin++ namespace adapter. Here
+# we're just declaring it (as it happens, IDefaultBrowserLayer
+# is a sub-type of IBrowserRequest)
+@interface.implementer(zope.publisher.interfaces.browser.IBrowserRequest,
+					   zope.publisher.interfaces.browser.IDefaultBrowserLayer)
 @component.adapter(pyramid.interfaces.IRequest)
 class PyramidZopeRequestProxy(SpecificationDecoratorBase):
 	"""
 	Makes a Pyramid IRequest object look like a Zope request
 	for purposes of rendering. The existing interfaces (IRequest) are preserved.
 
+	Changes to a proxy, including annotations, are persistent, and
+	will be reflected if the same pyramid request is proxied again
+	later (unlike :mod:`pyramid_zope_request`, which takes the approach of
+	subclassing :class:`zope.publisher.base.BaseRequest` and overriding
+	certain methods to call through to pyramid, but not things
+	like annotations.)
+
 	.. note:: Most of this behaviour is added from reverse-engineering what
 		existing zope code, most notably :mod:`z3c.table.table` uses.
+		Some additional support for :mod:`z3c.form` comes from
+		looking at what :mod:`pyramid_zope_request` does.
 	"""
 
 	def __init__( self, base ):
 		SpecificationDecoratorBase.__init__( self, base )
+		if getattr( base, 'registry', None ) is None:
+			base.registry = component.getSiteManager()
+
 		base.response.getHeader = lambda k: base.response.headers[k]
-		base.response.setHeader = lambda k, v, literal=False: operator.setitem( base.response.headers, str(k), str(v) if isinstance(v,unicode) else v )
+
+		def setHeader(name, value, literal=False):
+			# Go to bytes for python 2 if incoming was a string
+			name = str(name)
+			value = str(value) if isinstance(value,unicode) else value
+			if name.lower() == 'content-type':
+				# work around that webob stores the charset
+				# in the header ``Content-type``, zope kills the charset
+				# by setting e.g. ``text/html`` without charset
+				charset = base.response.charset
+				base.response.headers[name] = value
+				# restore the old charset
+				base.response.charset = charset
+			else:
+				base.response.headers[name] = value
+		base.response.setHeader = setHeader
+		base.response.addHeader = setHeader
+
+		base.response.getStatus = lambda: base.response.status_code
+		base.response.setStatus = lambda status_code: setattr(base.response, 'status_code', status_code )
 
 	@non_overridable
 	def get( self, key, default=None ):
@@ -52,6 +95,9 @@ class PyramidZopeRequestProxy(SpecificationDecoratorBase):
 		the WSGI environ, making the request dict-like for the environ.
 		Hence the need to mark this method non_overridable.
 		"""
+		# Zope does this by actually processing the inputs
+		# into a "form" object
+
 		def _d_o_l( o ):
 			return o.dict_of_lists() if hasattr( o, 'dict_of_lists' ) else o.copy() # DummyRequest GET/POST are different
 		dict_of_lists = _d_o_l( self.GET )
@@ -65,13 +111,48 @@ class PyramidZopeRequestProxy(SpecificationDecoratorBase):
 			val = self.environ.get( key, default )
 		return val
 
+	def items(self):
+		result = {}
+		result.update( self.environ )
+		result.update( self.GET )
+		result.update( self.POST )
+		return result.items()
+
+	def keys(self):
+		return [k for k, _ in self.items()]
+
+	def values(self):
+		return [v for _, v in self.items()]
+
+	def __iter__(self):
+		return iter(self.keys())
+
+	def __len__(self):
+		return len(self.items())
+
+	def __contains__(self, key):
+		return key in self.keys()
+
+	def __getitem__(self, key):
+		result = self.get(key, self)
+		if result is self:
+			raise KeyError(key)
+		return result
+
 	def getHeader(self, name, default=None):
 		return self.headers.get( name, default )
 
+	def getURL(self):
+		return self.path_url
 
 	@property
 	def locale(self):
-		return locales.getLocale( *get_locale_name( self ).split( '-' ) )
+		try:
+			# Country is optional
+			lang_country = get_locale_name( self ).split( '-' )
+		except AttributeError: # Testing, registry has no settings
+			lang_country = ('en', 'US')
+		return locales.getLocale( *lang_country )
 
 	@property
 	def annotations(self):
@@ -82,3 +163,49 @@ class PyramidZopeRequestProxy(SpecificationDecoratorBase):
 	def _set__annotations__(self, val):
 		getProxiedObject(self).__dict__['__annotations__'] = val
 	__annotations__ = property(_get__annotations__, _set__annotations__)
+
+	environment = alias('environ')
+
+	@property
+	def bodyStream(self):
+		return self.body_file_seekable
+
+	def _unimplemented(self,*args,**kwargs):
+		raise NotImplementedError()
+	@property
+	def _unimplemented_prop(self):
+		return NotImplemented
+
+	setPathSuffix = _unimplemented
+	getTraversalStack = _unimplemented
+	setTraversalStack = _unimplemented
+	processInputs = _unimplemented
+	publication = _unimplemented_prop
+	setPublication = _unimplemented
+	getPositionalArguments = _unimplemented
+	retry = _unimplemented
+	hold = _unimplemented
+	setupLocale = _unimplemented
+	traverse = _unimplemented
+	close = _unimplemented
+	debug = False
+	def supportsRetry(self):
+		return False
+
+	# This is supposed to be an IParticipation;
+	# we could almost do that
+	setPrincipal = _unimplemented
+
+	@property
+	def principal(self):
+		try:
+			return getInteraction().participations[0].principal
+		except (NoInteraction,IndexError,AttributeError):
+			return component.queryUtility(IUnauthenticatedPrincipal)
+
+	@property
+	def interaction(self):
+		try:
+			return getInteraction()
+		except NoInteraction:
+			return None
