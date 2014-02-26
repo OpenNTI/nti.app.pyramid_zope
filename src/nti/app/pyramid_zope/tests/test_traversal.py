17a311d001c4dc6e735a841ef472008294ab7951
Jason Madden <jason.madden@nextthought.com>
2014-02-26 16:14:09 +0000

    Tests

--- src/nti/app/pyramid_zope/tests/test_traversal.py
+++ src/nti/app/pyramid_zope/tests/test_traversal.py
@@ -5,6 +5,9 @@ $Id$
 """
 from __future__ import print_function, unicode_literals
 
+#pylint: disable=R0904
+
+
 from zope import interface
 from zope.traversing import interfaces as trv_interfaces
 
@@ -12,27 +15,27 @@ from zope.traversing import interfaces as trv_interfaces
 #from pyramid.testing import DummyRequest
 from nti.app.testing.request_response import ByteHeadersDummyRequest as DummyRequest
 
-from nti.appserver import traversal
-import nti.testing.base
+from .. import traversal
 
-setUpModule = lambda: nti.testing.base.module_setup( set_up_packages=(nti.appserver,) )
-tearDownModule = nti.testing.base.module_teardown
+from nti.app.testing.layers import AppLayerTest
 
-def test_unicode_traversal():
+class TestTraversal(AppLayerTest):
 
-	@interface.implementer(trv_interfaces.ITraversable)
-	class BrokenTraversable(object):
-		raised = False
-		def traverse( self, name, furtherPath ):
-			BrokenTraversable.raised = True
-			getattr( self, u'\u2019', None ) # Raise unicode error
+	def test_unicode_traversal(self):
 
-	@interface.implementer(trv_interfaces.ITraversable)
-	class DirectTraversable(object):
-		def traverse( self, name, furtherPath ):
-			return BrokenTraversable()
+		@interface.implementer(trv_interfaces.ITraversable)
+		class BrokenTraversable(object):
+			raised = False
+			def traverse( self, name, furtherPath ):
+				BrokenTraversable.raised = True
+				getattr( self, u'\u2019', None ) # Raise unicode error
 
-	req = DummyRequest(path='/a/b/c')
-	req.matchdict = {'traverse': ('a','b','c')}
-	traversal.ZopeResourceTreeTraverser( DirectTraversable() )( req )
-	assert BrokenTraversable.raised
+		@interface.implementer(trv_interfaces.ITraversable)
+		class DirectTraversable(object):
+			def traverse( self, name, furtherPath ):
+				return BrokenTraversable()
+
+		req = DummyRequest(path='/a/b/c')
+		req.matchdict = {'traverse': ('a','b','c')}
+		traversal.ZopeResourceTreeTraverser( DirectTraversable() )( req )
+		assert BrokenTraversable.raised
