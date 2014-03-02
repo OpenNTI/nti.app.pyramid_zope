572d417b4ddf7f3c21747c91786fa8eb84db9cfd
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-03-02 23:28:42 +0000

    add 'division'

--- src/nti/app/pyramid_zope/traversal.py
+++ src/nti/app/pyramid_zope/traversal.py
@@ -5,7 +5,7 @@ Support for resource tree traversal.
 
 $Id$
 """
-from __future__ import print_function, unicode_literals, absolute_import
+from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 from pyramid import traversal
