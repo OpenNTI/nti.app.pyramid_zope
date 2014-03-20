95ba929ade862b9c9e653303ee8fc5364c09b2b4
Jason Madden <jason.madden@nextthought.com>
2014-03-20 15:55:22 +0000

    Quick hack to let viewlets and registered templates auto-reload and/or debug
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.pyramid_zope/trunk@35195 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/pyramid_zope/__init__.py
+++ src/nti/app/pyramid_zope/__init__.py
@@ -20,22 +20,31 @@ from z3c.template import template
 from zope.pagetemplate.pagetemplatefile import package_home
 from six import string_types
 from .z3c_zpt import ViewPageTemplateFile
+import os
+# Best to use a class not a function to avoid changing
+# calling depth
+class _VPT(ViewPageTemplateFile):
+	def __init__(self, filename, _prefix=None, content_type=None):
+		path = _prefix
+		if not isinstance(path, string_types) and path is not None:
+			# zope likes to pass the globals
+			path = package_home(path)
+		# TODO: The correct values for reload and debug come from
+		# pyramid settings. Can we get to that from here?
+		auto_reload = os.getenv('PYRAMID_RELOAD_TEMPLATES')
+		debug = os.getenv('PYRAMID_DEBUG_TEMPLATES')
+		ViewPageTemplateFile.__init__(self, filename, path=path, content_type=content_type,
+									  auto_reload=auto_reload,
+									  debug=debug)
+
+
 if viewlet.ViewPageTemplateFile is viewpagetemplatefile.ViewPageTemplateFile:
 	# TODO: Formalize this
 	logger.debug("Monkey-patching zope.viewlet to use z3c.pt")
-	# Best to use a class not a function to avoid changing
-	# calling depth
-	class _VPT(ViewPageTemplateFile):
-		def __init__(self, filename, _prefix=None, content_type=None):
-			path = _prefix
-			if not isinstance(path, string_types) and path is not None:
-				# zope likes to pass the globals
-				path = package_home(path)
-			ViewPageTemplateFile.__init__(self, filename, path=path, content_type=content_type)
 
 	viewlet.ViewPageTemplateFile = _VPT
 if template.ViewPageTemplateFile is viewpagetemplatefile.ViewPageTemplateFile:
 	# They claim that including of z3c.ptcompat does this, I'm not
 	# convinced
 	logger.debug("Monkey-patching z3c.template to use z3c.pt")
-	template.ViewPageTemplateFile = ViewPageTemplateFile
+	template.ViewPageTemplateFile = _VPT
